57c9172758d2d2484eeb205fabf289b87156427a
Tim Hribar <hribartim@gmail.com>
2019-05-30 18:12:57 +0200

    Jinja use improved

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
 
+from flask import Flask, render_template
+import datetime
 app = Flask(__name__)
 
 @app.route ("/")
 def index():
-    return render_template("index.html")
+    some_text = "Message from the handler."
+    current_year = datetime.datetime.now().year
+    cities = ["Ljubljana", "Kranj", "Koper", "Ptuj"]
+    logged_in = True
+    return render_template("index.html", some_text=some_text, current_year=current_year, cities=cities, logged_in=logged_in)
 
 @app.route("/about")
 def about():
